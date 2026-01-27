abb73c1a8ab482f5bc4d6803aee7eac504c9f988
Vespine <142816007+VespineSpring@users.noreply.github.com>
2026-01-27 17:00:16 +0530

    Created Static Spike!

--- scenes/killzone.gd
+++ scenes/killzone.gd
@@ -4,7 +4,8 @@ extends Area2D
 @onready var timer: Timer = $Timer
 
 
-func _on_body_entered(_body: Node2D) -> void:
+func _on_body_entered(body: Node2D) -> void:
+    body.queue_free()
     timer.start()
 
 
