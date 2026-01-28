dada3bbadc2bd3c339b79c31136c0a2e3c8dcd9b
Vespine <142816007+VespineSpring@users.noreply.github.com>
2026-01-28 13:15:20 +0530

    Coded Key

--- scenes/key.gd
+++ scenes/key.gd
@@ -1,5 +1,10 @@
 extends Area2D
 
 
+@onready var game_manager: Node = %GameManager
+
+
 func _on_body_entered(_body: Node2D) -> void:
+    game_manager.keys += 1
+    print("Keys: ", game_manager.keys)
     queue_free()
