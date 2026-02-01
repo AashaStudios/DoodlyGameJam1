39dc9fe61b08053cf539d34f1b39879679fb60b1
Vespine <142816007+VespineSpring@users.noreply.github.com>
2026-02-01 18:13:52 +0530

    Made Interact to Pick Key

--- scenes/key.gd
+++ scenes/key.gd
@@ -1,9 +1,14 @@
-extends Area2D
+extends Node2D
 
 
 @onready var game_manager: Node = %GameManager
+@onready var interaction_area: InteractionArea = $InteractionArea
 
 
-func _on_body_entered(_body: Node2D) -> void:
+func _ready() -> void:
+    interaction_area.interact = Callable(self, "_on_interact")
+
+
+func _on_interact():
     game_manager.keys += 1
     queue_free()
