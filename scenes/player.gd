0c229a63985d5f08438aa2d6933d60acf0fe761b
Vespine <142816007+VespineSpring@users.noreply.github.com>
2026-02-03 13:09:17 +0530

    Removed Dash & Moving Spikes

--- scenes/player.gd
+++ scenes/player.gd
@@ -5,25 +5,17 @@ const SPEED: int = 200
 const ACCELERATION: int = 10
 const FRICTION: int = 15
 const JUMP_VELOCITY: int = -300
-const DASH_VELOCITY: int = 500
 
 var coyote_time_activated: bool = false
 
 @onready var sprite: Sprite2D = $Sprite2D
 @onready var jump_buffer_timer: Timer = $Timer/JumpBufferTimer
 @onready var coyote_timer: Timer = $Timer/CoyoteTimer
-@onready var dash_timer: Timer = $Timer/DashTimer
-@onready var dash_cooldown_timer: Timer = $Timer/DashCooldownTimer
 
 
 func _physics_process(delta: float) -> void:
     if not is_on_floor():
         velocity += get_gravity() * delta
-    
-    dash()
-    
-    if not dash_timer.is_stopped():
-        return
     
     var direction := Input.get_axis("left", "right")
     
@@ -37,21 +29,6 @@ func _physics_process(delta: float) -> void:
 func run(delta: float, direction: float) -> void:
     var body_weight: float = delta * (ACCELERATION if direction else FRICTION)
     velocity.x = lerp(velocity.x, direction * SPEED, body_weight)
-
-
-func dash() -> void:
-    if Input.is_action_just_pressed("dash") and dash_cooldown_timer.is_stopped():
-        dash_timer.start()
-        dash_cooldown_timer.start()
-    
-    if not dash_timer.is_stopped():
-        velocity.y = 0
-        if sprite.flip_h == false:
-            velocity.x = DASH_VELOCITY
-        else:
-            velocity.x = -DASH_VELOCITY
-        
-        move_and_slide()
 
 
 func jump() -> void:
